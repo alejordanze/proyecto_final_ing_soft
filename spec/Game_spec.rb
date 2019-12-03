682ccc781c3194c7b57af6d248965637a54895d9
IvySaskia <saskiaivy@outlook.com>
2019-12-02 23:14:58 -0400

    other tests

--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe Game do
     it 'It returns 1 when width=1 and length=1' do
         expect(@game.define_surface_size(1,1)).to eq(1)
     end
+
+    it 'It returns 1 when width=2 and length=1' do
+        expect(@game.define_surface_size(2,1)).to eq(2)
+    end
+
+    it 'It returns 1 when width=5 and length=2' do
+        expect(@game.define_surface_size(5,2)).to eq(10)
+    end
     
 end
