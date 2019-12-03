fa27e42363aa349650286d0bf8c8a5734e865478
IvySaskia <saskiaivy@outlook.com>
2019-12-03 09:00:32 -0400

    other functions

--- lib/Game.rb
+++ lib/Game.rb
@@ -3,5 +3,33 @@ class Game
     def define_surface_size(width,length)
         width*length
     end
+
+    def define_initial_position(x,y)
+        return x, y
+    end
+
+    def turn_left()
+        'turning left'
+    end
+
+    def turn_right()
+        'turning right'
+    end
+
+    def give_a_step_forward()
+        'moving forward'
+    end
+
+    def final_position()
+        return 0,0
+    end
+
+    def travel(cad)
+        cad
+    end
+    
+
+
+    
     
 end
--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -17,5 +17,49 @@ RSpec.describe Game do
     it 'It returns 1 when width=5 and length=2' do
         expect(@game.define_surface_size(5,2)).to eq(10)
     end
+
+    it 'It returns x=1 and y=1' do
+        expect(@game.define_initial_position(1,1)).to eq([1,1])
+    end
+
+    it 'It returns x=2 and y=5' do
+        expect(@game.define_initial_position(2,5)).to eq([2,5])
+    end
+
+    it 'It returns "turn left" when we call turn_left' do
+        expect(@game.turn_left()).to eq('turning left')
+    end
+
+    it 'It returns "turn right" when we call turn_right' do
+        expect(@game.turn_right()).to eq('turning right')
+    end
     
+    it 'It returns "moving forward" when we call give_a_step_forward' do
+        expect(@game.give_a_step_forward()).to eq('moving forward')
+    end
+    
+    it 'It returns "moving forward" when we call give_a_step_forward' do
+        expect(@game.give_a_step_forward()).to eq('moving forward')
+    end
+
+    it 'It returns the final position as 0,0 when we want to se the final position' do
+        expect(@game.final_position()).to eq([0,0])
+    end
+
+    it 'It returns L when the instructions for traveling is L' do
+        expect(@game.travel('L')).to eq('L')
+    end
+
+    it 'It returns R when the instructions for traveling is R' do
+        expect(@game.travel('R')).to eq('R')
+    end
+
+    it 'It returns LR when the instructions for traveling is LR' do
+        expect(@game.travel('LR')).to eq('LR')
+    end
+
+    it 'It returns LRF when the instructions for traveling is LRF' do
+        expect(@game.travel('LRF')).to eq('LRF')
+    end
+
 end
