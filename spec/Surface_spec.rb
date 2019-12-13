ebe731ff28b7dc9b06f6f5fdc88b7f97ecf9de9c
IvySaskia <saskiaivy@outlook.com>
2019-12-13 00:30:54 -0400

    tests of is_valid_dimensions

--- spec/Surface_spec.rb
+++ spec/Surface_spec.rb
@@ -17,5 +17,24 @@ RSpec.describe Surface do
         @surface.set_surface(0,1)
         expect(@surface.get_rows()).to eq(1)
     end
+    
+    it 'It returns true when columns are 1 and rows are 1' do
+        @surface.set_surface(1,1)
+        expect(@surface.is_valid_dimensions?()).to eq(true)
+    end
 
+    it 'It returns false when columns are 1 and rows are 0' do
+        @surface.set_surface(1,0)
+        expect(@surface.is_valid_dimensions?()).to eq(false)
+    end
+
+    it 'It returns false when columns are 0 and rows are 0' do
+        @surface.set_surface(0,0)
+        expect(@surface.is_valid_dimensions?()).to eq(false)
+    end
+
+    it 'It returns false when columns are -1 and rows are -1' do
+        @surface.set_surface(-1,-1)
+        expect(@surface.is_valid_dimensions?()).to eq(false)
+    end
 end
